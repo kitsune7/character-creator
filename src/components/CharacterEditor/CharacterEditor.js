a429225c28bc5a198a9f8b3e38237352b608baac
Christopher Bradshaw <chris.kofox@gmail.com>
2024-03-22 16:02:47 -0600

    extra credit!

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { defaultSkinColor, defaultClothesColor } from '../../constants'
 import Character from '../Character'
@@ -14,6 +14,7 @@ import {
   clothesColorOptions,
 } from './CharacterEditor.helpers'
 import styles from './CharacterEditor.module.css'
+import mobileStyles from './MobileCharacterEditor.module.css'
 
 function App() {
   const [body, setBody] = React.useState(0)
@@ -23,53 +24,95 @@ function App() {
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor)
   const [clothesColor, setClothesColor] = React.useState(defaultClothesColor)
 
+  // I could definitely optimize this by selectively changing CSS properties with media queries,
+  // but that's a lot of work for an extra credit portion of a workshop I'm doing for myself.
   return (
-    <main className={styles.characterEditor}>
-      <div className={styles.perspective} />
-      <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <header className={styles.header}>
-          <h1 className={styles.title}>Create your Character</h1>
-          <p className={styles.description}>
-            Customize your character's look and style using the controls below. What sort of adventure will you embark
-            on?{' '}
-          </p>
-        </header>
-        <div className={styles.controlColumn}>
-          <ControlPane title="Bodies" options={bodyOptions} currentOption={body} handleSelectOption={setBody} />
-          <ControlPane title="Heads" options={headOptions} currentOption={head} handleSelectOption={setHead} />
-          <ControlPane title="Faces" options={faceOptions} currentOption={face} handleSelectOption={setFace} />
-          <ControlPane
-            title="Accessories"
-            options={accessoryOptions}
-            currentOption={accessory}
-            handleSelectOption={setAccessory}
-          />
-          <ControlPane
-            title="Skin Color"
-            options={skinColorOptions}
-            currentOption={skinColor}
-            handleSelectOption={setSkinColor}
-          />
-          <ControlPane
-            title="Clothing Color"
-            options={clothesColorOptions}
-            currentOption={clothesColor}
-            handleSelectOption={setClothesColor}
+    <Fragment>
+      <main className={mobileStyles.characterEditor}>
+        <div className={mobileStyles.characterWrapper}>
+          <Character
+            body={body}
+            head={head}
+            face={face}
+            accessory={accessory}
+            skinColor={skinColor}
+            clothesColor={clothesColor}
           />
         </div>
-      </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
-    </main>
+        <div className={mobileStyles.controlScrollArea}>
+          <div className={mobileStyles.controlColumn}>
+            <ControlPane title="Bodies" options={bodyOptions} currentOption={body} handleSelectOption={setBody} />
+            <ControlPane title="Heads" options={headOptions} currentOption={head} handleSelectOption={setHead} />
+            <ControlPane title="Faces" options={faceOptions} currentOption={face} handleSelectOption={setFace} />
+            <ControlPane
+              title="Accessories"
+              options={accessoryOptions}
+              currentOption={accessory}
+              handleSelectOption={setAccessory}
+            />
+            <ControlPane
+              title="Skin Color"
+              options={skinColorOptions}
+              currentOption={skinColor}
+              handleSelectOption={setSkinColor}
+            />
+            <ControlPane
+              title="Clothing Color"
+              options={clothesColorOptions}
+              currentOption={clothesColor}
+              handleSelectOption={setClothesColor}
+            />
+          </div>
+        </div>
+      </main>
+      <main className={styles.characterEditor}>
+        <div className={styles.perspective} />
+        <MaxWidthWrapper className={styles.maxWidthWrapper}>
+          <header className={styles.header}>
+            <h1 className={styles.title}>Create your Character</h1>
+            <p className={styles.description}>
+              Customize your character's look and style using the controls below. What sort of adventure will you embark
+              on?{' '}
+            </p>
+          </header>
+          <div className={styles.controlColumn}>
+            <ControlPane title="Bodies" options={bodyOptions} currentOption={body} handleSelectOption={setBody} />
+            <ControlPane title="Heads" options={headOptions} currentOption={head} handleSelectOption={setHead} />
+            <ControlPane title="Faces" options={faceOptions} currentOption={face} handleSelectOption={setFace} />
+            <ControlPane
+              title="Accessories"
+              options={accessoryOptions}
+              currentOption={accessory}
+              handleSelectOption={setAccessory}
+            />
+            <ControlPane
+              title="Skin Color"
+              options={skinColorOptions}
+              currentOption={skinColor}
+              handleSelectOption={setSkinColor}
+            />
+            <ControlPane
+              title="Clothing Color"
+              options={clothesColorOptions}
+              currentOption={clothesColor}
+              handleSelectOption={setClothesColor}
+            />
+          </div>
+        </MaxWidthWrapper>
+
+        <div className={styles.characterWrapper}>
+          <Character
+            body={body}
+            head={head}
+            face={face}
+            accessory={accessory}
+            skinColor={skinColor}
+            clothesColor={clothesColor}
+          />
+        </div>
+      </main>
+    </Fragment>
   )
 }
 
